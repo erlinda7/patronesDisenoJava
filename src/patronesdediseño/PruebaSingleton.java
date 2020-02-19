3b6136fc5ed4bba37f42a290313a9a901b1f93e8
Erlinda <'erlindachambimanzanoerlinda@gamil.com'>
2020-02-19 08:30:56 -0400

    varios hilos de ejecucion

---
+++ src/patronesdediseño/PruebaSingleton.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package patronesdediseño;
+
+/**
+ *
+ * @author chamb
+ */
+public class PruebaSingleton {
+
+
+    public static void main(String[] args) {
+       //Singleton miSingleton = new Singleton();
+        Singleton miSingleton = Singleton.obtenerSingleton(); //solo se crea el primero los demas ya no
+        Singleton miSingleton2 = Singleton.obtenerSingleton();
+        Singleton miSingleton3 = Singleton.obtenerSingleton();
+        Singleton miSingleton4= Singleton.obtenerSingleton();
+        Singleton miSingleton5 = Singleton.obtenerSingleton();
+        Singleton miSingleton6 = Singleton.obtenerSingleton();
+        
+        miSingleton6.vecesLlamado();
+        System.out.println("He terminado de crear los objetos");
+    }
+    
+}
---
+++ src/patronesdediseño/Singleton.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package patronesdediseño;
+
+/**
+ *
+ * @author chamb
+ */
+//creacion
+//Asegurarse de tener solo un objeto para todas las solicitudes
+//No permitir crear mas de un objeto asi algun programa lo solicite
+public final class Singleton {
+    
+    private final static Singleton singleton =new Singleton();
+    private static int cantidad;
+    
+    private Singleton(){
+        System.out.println("Hola, he sido creado una sola vez!!!");
+    }
+    public static Singleton obtenerSingleton(){
+        cantidad++;
+        return singleton;
+    }
+    
+    public static void vecesLlamado(){
+        System.out.println("Se ha llamado el metodo: " +cantidad +"veces");
+    }
+}
